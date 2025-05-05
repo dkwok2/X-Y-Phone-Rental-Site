40301e719d1b02117f178258a8225314691ec6a5
dkwok2 <160056755+dkwok2@users.noreply.github.com>
2025-05-04 22:43:45 -0500

    Update changes
    Added new features

---
+++ XandYPhoneRental/README.md
@@ -0,0 +1,16 @@
+:globe_with_meridians: CSCE 464 Web Project (Version 1.0)
+
+Note: Completed sections are mainly located in index.html and rateus.html. Other sections require improvement and further development but features include:
+- A responsive star rating on rateus.html
+- A typing animation on both index and rateus
+- Working links to all pages except follow us on the footer
+- A slide-up-down animation for the phone on index
+
+
+
+TODO:
+- Add a form page(both contact and rating) (Medium Priority)
+- Add a media query for responsiveness (HIGH PRIORITY)
+- Find ways to store forms and ratings (Low Priority)
+- Add a toggle day/night mode
+- Add a thank you for participating page
---
+++ XandYPhoneRental/scripts/rating.js
@@ -0,0 +1,67 @@
+// Scrpt file for rateus.html
+// Includes a star evaluation and a check completion method
+var stars = document.querySelectorAll(".rating i");
+var commentBox = document.getElementById("comment");
+var emailInput = document.getElementById("email");
+var nameInput = document.getElementById("name");
+var form = document.querySelector(".eval-form");
+let ratingValue = 0;
+
+// Function to count stars inputted by users
+function countstars(){
+    for (let i = 0; i < stars.length; i++) {
+        // Create a function clickstars where the user clicks the star image
+        stars[i].onclick = function clickstars() {
+            ratingValue = i + 1;
+            updateStars(ratingValue);
+        };
+    }
+}
+
+// Function to color stars based on the input
+function updateStars(rating) {
+    for (let i = 0; i < stars.length; i++) {
+        if (i < rating) {
+            stars[i].style.color = "#002266";
+        } else {
+            stars[i].style.color = "#ccc";
+        }  
+    }
+    document.getElementById("rating").value = rating;
+}
+
+function verifyCompletion(){
+    // Check to see if contents are empty
+    if (nameInput.value.trim() == "" || emailInput.value.trim() == ""|| ratingValue === 0) {
+        alert("Please fill in all fields and select a rating.");
+        return false;
+    }
+    return true;
+}
+
+
+function sendCompletion(){
+    let totalWords = commentBox.value.trim().split(/\s+/).length
+    alert(`Thank you for ${totalWords} words of feedback!`);
+}
+
+function resetValues(){
+    nameInput.value = "";
+    emailInput.value = "";
+    commentBox.value = "";
+    ratingValue = 0;
+    updateStars(ratingValue);
+}
+
+
+// Javascipt main method
+
+countstars();
+
+form.addEventListener("submit", function(event){
+    if (!verifyCompletion()) {
+        event.preventDefault();
+        return;
+    }
+    sendCompletion();
+});
